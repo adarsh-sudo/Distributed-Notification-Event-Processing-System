8f761626f46ccdf91fb6d6bf86d9885cb31aafe7
adarsh-sudo <writetoadarshsoni@gmail.com>
2026-03-22 15:57:27 +0530

    Added services.py and added business logic here

---
+++ apps/notifications/services.py
@@ -0,0 +1,22 @@
+from .models import Event, Notification
+from .tasks import send_notification
+import logging
+logger = logging.getLogger(__name__)
+
+def process_event(event: Event):
+    """
+    Handles event → notification → async trigger
+    """
+    logger.info(f"Processing event {event.id} of type {event.event_type} for user {event.user_id}")
+    # 1. Create Notification
+    notification = Notification.objects.create(
+        user=event.user,
+        event=event,
+        notification_type='EMAIL',
+        message=f"{event.event_type} triggered"
+    )
+
+    # 2. Trigger async task
+    send_notification.delay(notification.id)
+
+    return notification
